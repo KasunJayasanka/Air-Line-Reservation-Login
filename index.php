f724cfce64f2050865f527a74114f3b975eae3fe
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2022-10-07 09:24:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>Login</title>
+</head>
+<body>
+	<div class="container">
+		<div class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Welcome!</p>
+            <p class="login-text" style="font-size: 1.5rem; font-weight: 700;text-align: left;color:rgba(209, 211, 219, 0.751);">I am a,</p>
+			<div class="input-group">
+                <a href="signInPassenger.php" style="text-decoration:none ;">
+				<button name="Passenger" class="btn">Passenger</button>
+                </a>     
+			</div>
+            <div class="input-group">
+                <a href="signInStaff.php" style="text-decoration:none ;">
+                <button name="Staff" class="btn" id="s">Staff</button>
+                </a>
+            </div>
+		</div>
+	</div>
+</body>
+</html>
