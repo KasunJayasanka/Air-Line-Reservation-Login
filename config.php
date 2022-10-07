6e957a9350bf2cf449b613cbf68b5dc6d57a5ae8
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2022-10-07 12:42:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $server = "us-cdbr-east-06.cleardb.net";
 $user = "bd66653b4b6b6b";
 $pass = "f76d5d92";
-$database = "usersbase";
+$database = "heroku_0f865aaa02e660b";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
